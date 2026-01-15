65fac73de562a1561aa15f0c2642633d33067117
Dat9999999 <huynhtandat184@gmail.com>
2026-01-15 14:27:00 +0700

    Initial commit

---
+++ src/main/java/com/example/biteflow/BiteFlowApplication.java
@@ -0,0 +1,13 @@
+package com.example.biteflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiteFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiteFlowApplication.class, args);
+	}
+
+}
